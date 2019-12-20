54bc03fc7ff56112b07cd4acec67a154c2310422
BarakinSergey <55586597+BarakinSergey@users.noreply.github.com>
2019-12-20 20:05:57 +0300

    Add files via upload

--- last_central_v1.py
+++ last_central_v1.py
@@ -11,6 +11,10 @@ canv.pack(fill=BOTH,expand=1)
 #---------------------------------------------------------------menu_itself------------------------------------------------------------------------------------------- 
 enter_exam = 0
 number = 1
+person = []
+result = []
+person_text = []
+result_text = []
 
 # ---------------------------------------------------------------creating characters and giving them their unics-------------------------------------------------------------------------------------------
 class out:
@@ -120,7 +124,7 @@ def hit_test(obj_1, obj_2):
 			obj_1.v_y = obj_2.v_y
 			obj_2.v_y = change
 		if (obj_1.v_x - obj_2.v_x)**2 != 0:
-			if obj_1 == car_left or obj_1 == car_right:
+			if obj_1 == car_left:
 				change = obj_1.v_x
 				obj_1.v_x = obj_2.v_x
 				obj_2.v_x = change
@@ -130,7 +134,60 @@ def hit_test(obj_1, obj_2):
 	else:
 		return False
 
-def update():
+def play_view_one():
+	font = canv.create_rectangle(0, 0, 1500, 800, fill='orange')
+	road = canv.create_rectangle(300, 0, 1200, 800, fill='gray')
+	# car left
+	com_1 = canv.create_rectangle(0, 650, 300, 800, fill='gray', outline='red', width=4)
+	com_text_1 = canv.create_text((0 + 300) / 2, (700 + 800) / 2 - 70, text="RED car's statistics:", justify=CENTER,
+								  font="Verdana 14", fill='black')
+	com_distance_1 = canv.create_text((0 + 300) / 2 - 50, (700 + 800) / 2 - 40, text="reached distance:", font="Verdana 14",
+								  fill='black')
+	com_fuel_1 = canv.create_text((0 + 300) / 2 - 85, (700 + 800) / 2 - 10, text="fuel level:", font="Verdana 14",
+								  fill='black')
+	com_wheels_1 = canv.create_text((0 + 300) / 2 - 50, (700 + 800) / 2 + 20, text="wheel's hardness:",
+									font="Verdana 14", fill='black')
+
+def new_game_one():
+	global car_left, cars_ord, out_left, out_right, com_text_distance_1, key_code_left, t0
+	root.bind('<KeyPress>', key_pression)
+	while car_left.k_fuel*car_left.k_wheels != 0:
+		for car_ord in cars_ord:
+			car_ord.move_ordinary()
+			if hit_test(car_ord, car_left): #последовательность важна 
+				car_left.properties(0, 1)
+		lost_time = int(time.time() - t0)
+		velocity_sq_mid = 1
+		if car_left.k_fuel != 0:
+			car_left.distance = (lost_time * velocity_sq_mid) + (800 - car_left.y)/100
+		if (car_left.distance*10)//1/10 == car_left.distance:
+			canv.delete(com_text_distance_1)
+			com_text_distance_1 = canv.create_text((0 + 300) / 2 + 70, (700 + 800) / 2 - 40, text=str((car_left.distance*10)//1/10), font="Verdana 14",
+									  fill='black')
+		update_one()
+
+def update_one():
+	global car_left, cars_ord, out_left, out_right, com_text_distance_1, key_code_left, t0
+	car_left.movement_control(key_code_left[0])
+	if car_left.k_wheels == 0:
+		car_left.fuel = 0
+		car_left.move_ordinary()
+		if car_left.y > 800 + car_left.length:
+			canv.delete(car_left.obj)
+			car_left.obj = canv.create_oval(
+                -100 - 0,
+                -100 - 0,
+                -100 - 0,
+                -100 - 0,
+                fill='black', width = 0)
+	for i in range(0, len(key_code_left)-1):
+		key_code_left[i] = key_code_left[i+1]
+	canv.delete(key_code_left[len(key_code_left)-1])
+	#print(key_code_left, key_code_right)
+	canv.update()
+
+
+def update_two():
 	global car_left, car_right, cars_ord, out_left, out_right, com_text_distance_1, com_text_distance_2, key_code_left, key_code_right, t0
 	car_left.movement_control(key_code_left[0])
 	car_right.movement_control(key_code_right[0])
@@ -176,7 +233,7 @@ def key_pression(event):
 		key_code_right.append(key_code)
 
 # ---------------------------------------------------------------view of a background: road, font and two tables of statictics-------------------------------------------------------------------------------------------
-def play_view():
+def play_view_two():
 	font = canv.create_rectangle(0, 0, 1500, 800, fill='orange')
 	road = canv.create_rectangle(300, 0, 1200, 800, fill='gray')
 	# car left
@@ -201,7 +258,7 @@ def play_view():
 	                                    justify=CENTER, font="Verdana 14", fill='black')
 
 # ---------------------------------------------------------------interaction with players: checking buuttons press, moving cars and checking them touching each other and road border-------------------------------------------------------------------------------------------
-def new_game():
+def new_game_two():
 	global car_left, car_right, cars_ord, out_left, out_right, com_text_distance_1, com_text_distance_2, key_code_left, key_code_right, t0
 	root.bind('<KeyPress>', key_pression)
 	while car_left.k_fuel*car_left.k_wheels + car_right.k_fuel*car_right.k_wheels != 0:
@@ -225,7 +282,7 @@ def new_game():
 			canv.delete(com_text_distance_2)
 			com_text_distance_2 = canv.create_text((1200 + 1500) / 2 + 70, (700 + 800) / 2 - 40, text=str((car_right.distance*10)//1/10), font="Verdana 14",
 									  fill='black')
-		update()
+		update_two()
 
 #-------------------------------------------------------------------central part----------------------------------------------------------------------------
 
@@ -237,6 +294,25 @@ def last_instruction():
 
 def last_train():
 	print('last_train')
+	global car_left, cars_ord, out_left, out_right, com_text_distance_1, key_code_left, t0
+	key_code_left = []
+	key_code_left.append(0)
+	
+	play_view_one()
+
+	car_left = car('red', 750, 640, 0, 0)
+	cars_ord = [ car('brown', 330+60*rnd(1,15), -100-130*rnd(1,10), 0, 20) for i in range(1, 30+1)]
+	out_left = out(300, 400, 800, 4)
+	out_right = out(1200, 400, 800, 4)
+
+	#заставка
+	t0 = time.time()
+	lost_time = 0
+	car_left.distance = 0
+	com_text_distance_1 = canv.create_text((0 + 300) / 2 + 70, (700 + 800) / 2 - 40, text=str(car_left.distance), font="Verdana 14",
+										  fill='black')
+	new_game_one()	
+	canv.delete(car_left, cars_ord, out_left, out_right, com_text_distance_1)
 
 def last_play():
 	print('last_play')
@@ -246,7 +322,7 @@ def last_play():
 	key_code_right = []
 	key_code_right.append(0)
 
-	play_view()
+	play_view_two()
 
 	car_left = car('red', 450, 640, 0, 0)
 	car_right = car('blue', 1050, 640, 0, 0)
@@ -264,11 +340,40 @@ def last_play():
 										  fill='black')
 	com_text_distance_2 = canv.create_text((1200 + 1500) / 2 + 70, (700 + 800) / 2 - 40, text=str(car_right.distance), font="Verdana 14",
 										  fill='black')
-	new_game()
+	new_game_two()
+	canv.delete(car_left, car_right, cars_ord, out_left, out_right, com_text_distance_1, com_text_distance_2)
 
 def last_results():
+	global exit_results, head_text, person_text, result_text, exit_obj, exit_obj_text
 	print('last_results')
-
+	for i in range(1,12+1):
+		person.append(0)
+		result.append(0)
+	file = open('results.txt', 'r')
+	number = 0
+	for lines in file:
+		if number == (number//2)*2:
+			person[number//2] = lines
+		if number == (number//2)*2+1:
+			result[number//2-1] = lines
+		number += 1
+	#for i in range(0,12):
+	#	print(person[i], 'person ', result[i], 'result ')
+	file.close()
+	font = canv.create_rectangle(0, 0, 1500, 800, fill='orange')
+	road = canv.create_rectangle(300, 0, 1200, 800, fill='gray')
+	head_text = canv.create_text(750, 50, text="TABLE OF 10 GREATEST RESULTS\nCONGRATULATIONS TO THESE PLAYERS!",
+            fill='orange' , justify=CENTER, font="Verdana 20")
+	for i in range(0,10):
+		person_text.append(canv.create_text(500, 100+30*(1+i), text=str(person[i]),
+            fill="orange", font="Verdana 17"))
+		result_text.append(canv.create_text(1000, 100+30*(1+i), text=str(result[i-1]),
+            fill="orange", font="Verdana 17"))
+		exit_obj = canv.create_rectangle(670, 445, 830, 475, fill='white', outline='red', width=3)
+		exit_obj_text = canv.create_text(750, 460, text="return to menu",
+                fill="red" , justify=CENTER, font="Verdana 13")
+	canv.delete(person_text, result_text, exit_obj, exit_obj_text)
+	
 def last_skins():
 	print('last_skins')
 
@@ -304,10 +409,12 @@ def menu():
 	else:
 		button_exit = canv.create_rectangle(600,130+(inf_num)*50,900,180+(inf_num)*50, fill='orange')
 		text_exit = canv.create_text((600+900)/2, (130+180)/2+(inf_num)*50, text='exit', justify=CENTER, font="Verdana 14", fill='black')
+	root.bind('<Key>',keypress_menu)
 	
 def keypress_menu(event):
 	global button, number, inf_num, button_exit, button_text, button_text_show, enter_exam, num_exit
 	global window_exit, question_exit, exit_yes, exit_no
+	global head_text, person_text, result_text, exit_obj, exit_obj_text
 	#print(event.keycode, number)
 	if enter_exam == 0:
 		if event.keycode == 38:
@@ -343,10 +450,13 @@ def keypress_menu(event):
 					last_instruction()
 				if number == 2:
 					last_train()
+					menu()
 				if number == 3:
 					last_play()
+					menu()
 				if number == 4:
 					last_results()
+					canv.delete(head_text, person_text, result_text, exit_obj, exit_obj_text)
 				if number == 5:
 					last_skins()
 				if number == 6:
@@ -390,5 +500,4 @@ def keypress_menu(event):
 #--------------------------------------------------------------------------------------------------------------------------
 intro()	
 menu()
-root.bind('<Key>',keypress_menu)
 mainloop()
